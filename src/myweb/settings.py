9f9ad811514de71d1e4c19b36807dfdb14ce98e1
ethan <ethanlim99@gmail.com>
2020-11-21 20:06:31 -0600

    added django-heroku dependency to requirements and updated settings.py

--- src/myweb/settings.py
+++ src/myweb/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-from pathlib import Path
+import django_heroku
 import os
+from pathlib import Path
 from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -191,3 +192,5 @@ LOGGING = {
         },
     },
 }
+
+django_heroku.settings(locals())
